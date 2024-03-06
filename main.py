6faec51dbc73d8f4d6011251ff68d55198faef07
Tsering Dhargyal <160236431+dhargyalla@users.noreply.github.com>
2024-03-06 15:51:49 +0000

    First commit

--- main.py
+++ main.py
@@ -68,6 +68,24 @@ word_length = len(chosen_word)
 #Set 'lives' to equal 6.
 lives = 6
 
+=======
+#Step 5
+
+import random
+
+#TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
+#Delete this line: word_list = ["ardvark", "baboon", "camel"]
+from hangman_words import word_list
+chosen_word = random.choice(word_list)
+#chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
+from hangman_art import logo
+print(logo)
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
@@ -90,6 +108,21 @@ while not end_of_game:
     #Then reduce 'lives' by 1. 
     #If lives goes down to 0 then the game should stop and it should print "You lose."
     if guess not in chosen_word:
+    #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+    if guess not in display:
+        #Check guessed letter
+        for position in range(word_length):
+            letter = chosen_word[position]
+            print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+            if letter == guess:
+                display[position] = letter
+    else:
+      print(f"You have already guessed {guess}")
+
+    #Check if user is wrong.
+    if guess not in chosen_word:
+        #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+        print(f"You guessed {guess}, that's not in the chosen word. You lose a life.")
         lives -= 1
         if lives == 0:
             end_of_game = True
@@ -104,4 +137,6 @@ while not end_of_game:
         print("You win.")
 
     #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+    #TODO-2: - Import the stages from hangman_art.py and make this error go away.
+    from hangman_art import stages
     print(stages[lives])
